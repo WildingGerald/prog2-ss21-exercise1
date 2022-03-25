3cbfbe5e77f07d7e12fe4c2ac29be23016cfde01
adre <gerald.wilding@stud.fh-campuswien.ac.at>
2022-03-25 20:19:38 +0100

    add datafiles tests for Dataimport

--- src/main/java/rocketFuelCalculator.java
+++ src/main/java/rocketFuelCalculator.java
@@ -1,7 +1,16 @@
 public class rocketFuelCalculator {
-    public int calculateFuel(int mass)throws Exception{
-       int requiredFuel;
 
-       return 1;
+
+    public int calculateFuel(int mass)throws Exception{
+        int requiredFuel;
+        if (mass > 9)
+        {
+            double h =mass / 3d;
+            double b =Math.floor(h);
+            requiredFuel = (int)b - 2;
+        }else{
+            throw new Exception("Mass negative or too low to calculate fuel for.");
+        }
+       return requiredFuel;
     }
 }
--- src/test/java/TestRocketFuelCalculator.java
+++ src/test/java/TestRocketFuelCalculator.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,15 +14,35 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class TestRocketFuelCalculator {
 
     rocketFuelCalculator testRocket;
-    @BeforeEach
+    @BeforeAll
     void setUp(){
         testRocket = new rocketFuelCalculator();
     }
 
+    @Test
+    public void testImportData_filepathWrong(){
+        try{
+            testRocket.importData("wrongPath");
+            fail();
+        }catch (IOException e){
+
+        }
+    }
+
+    @Test
+    public void testImportData_pass(){
+        try{
+            testRocket.importData("resources/data");
+            Assertions.assertTrue(testRocket.modulesMass.contains(91617));
+        }catch (Exception e){
+            fail();
+        }
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {12 , 14, 1969, 100756})
-    public void testCalculateFuel_works(int in) {
-        List expected = Arrays.asList(new int[]{4, 4, 654, 33583});
+    public void testCalculateFuel_pass(int in) {
+        List expected = Arrays.asList(2, 2, 654, 33583);
         try {
             Assertions.assertTrue(expected.contains(testRocket.calculateFuel(in)));
         }catch (Exception e){
